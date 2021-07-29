a41c3d08d1e834a807d5aed9ebb919f119de651f
Gabrieleus <felix.felix.felix.000@gmail.com>
2021-07-29 01:16:52 -0500

    añadir Education.jsx
    para empezar a añadir las tablas de contenido

---
+++ src/components/Education.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Education = () => {
+    return (
+        <div>
+            <h2>Estudios</h2>
+            <div className="Education-container">
+                <div className="Education-items">
+                    <h1>ITM - <span>2019</span>
+                    <p>Ingenieria de Sistemas</p>
+                    </h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Education } from './components/Education';
 import App from './container/App'
+
 
 ReactDOM.render(
   
   <App />,
+  <Education />,
   document.getElementById('root')
 );
